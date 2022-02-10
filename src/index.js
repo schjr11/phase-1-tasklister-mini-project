290d487d27960260acf08093bc30ef9e8883a316
Scott Hill <scott.c.hill.jr@gmail.com>
2022-02-09 20:38:38 -0600

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#create-task-form").addEventListener('submit', (e) => {
+    e.preventDefault()
+    let li = document.createElement ('li')
+    li.textContent = e.target["new-task-description"].value
+    document.querySelector('#tasks').append(li)
+  })
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#create-task-form").addEventListener('submit', (e) => {
+    e.preventDefault()
+    let li = document.createElement ('li')
+    li.textContent = e.target["new-user-entry"].value
+    document.querySelector('#users').append(li)
+  })
 });
